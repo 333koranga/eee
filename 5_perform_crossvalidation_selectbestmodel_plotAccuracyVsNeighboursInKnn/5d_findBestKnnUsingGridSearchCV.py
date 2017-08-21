03e7745e5628c9e379c7fdc81d95b85dd861393e
333koranga <333koranga@gmail.com>
2017-08-21 13:20:53 +0530

    Update 5d_findBestKnnUsingGridSearchCV.py

--- 5_perform_crossvalidation_selectbestmodel_plotAccuracyVsNeighboursInKnn/5d_findBestKnnUsingGridSearchCV.py
+++ 5_perform_crossvalidation_selectbestmodel_plotAccuracyVsNeighboursInKnn/5d_findBestKnnUsingGridSearchCV.py
@@ -108,7 +108,7 @@ for k in kRange:
 	#print currentmean	
 	kScore.append(scores.mean())
 	fh=open(projectfolder+"rat","a")
-	fh.write(str(currentmean))
+	fh.write(str(currentmean)+"'")
 	fh.close()
 	t=(time.time() - thistime)/3600.0
 	print("---time required for this iteration : ",t)
